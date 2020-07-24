10ad356a9ce8b6f954f65f0b023b29f7682bb586
Stephan Diehl <stephan.diehl@opensolutions.net>
2020-07-25 00:41:45 +0200

    add courses findAll

---
+++ rest-api/src/courses/controllers/courses.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+import { Course } from '../../../../shared/course';
+import { findAllCourses } from '../../../db-data';
+
+@Controller()
+export class CoursesController {
+  @Get('/api/courses')
+  async findAllCourses(): Promise<Course[]> {
+    return findAllCourses();
+  }
+}
