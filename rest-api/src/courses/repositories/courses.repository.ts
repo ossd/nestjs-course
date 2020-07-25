340be344c2623c9b26b321a0c6d5e5cee7b90638
Stephan Diehl <stephan.diehl@opensolutions.net>
2020-07-25 16:47:51 +0200

    fix missing types

--- rest-api/src/courses/repositories/courses.repository.ts
+++ rest-api/src/courses/repositories/courses.repository.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { Course } from '../../../../shared/course';
 import { InjectModel } from '@nestjs/mongoose';
+import * as mongoose from 'mongoose';
 
 @Injectable()
 export class CoursesRepository {
-  constructor(@InjectModel('Course') private courseModel: Model<Course>) {}
+  constructor(
+    @InjectModel('Course')
+    private courseModel: Model<Course & mongoose.Document>,
+  ) {}
 
   async findAll(): Promise<Course[]> {
     return this.courseModel.find();
