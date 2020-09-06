f74033c6b65191c80a557d3df10509bf230a7409
Stephan Diehl <stephan.diehl@opensolutions.net>
2020-09-06 14:21:24 +0200

    add class validator and pipe

--- rest-api/src/courses/controllers/courses.controller.ts
+++ rest-api/src/courses/controllers/courses.controller.ts
@@ -7,6 +7,7 @@ import {
   Delete,
   Post,
   BadRequestException,
+  ParseIntPipe,
 } from '@nestjs/common';
 import { Course } from '../../../../shared/course';
 import { CoursesRepository } from '../repositories/courses.repository';
@@ -28,8 +29,11 @@ export class CoursesController {
   @Put(':courseId')
   async updateCourse(
     @Param('courseId') courseId: string,
+    @Body('seqNo', ParseIntPipe) seqNo: number,
     @Body() changes: Partial<Course>,
   ): Promise<Course> {
+    console.log('seqNo value: ' + seqNo + ', type: ' + typeof seqNo);
+
     if (changes._id) {
       throw new BadRequestException("Can't update course id");
     }
--- shared/course.ts
+++ shared/course.ts
@@ -1,28 +1,35 @@
+import { IsInt, IsString, IsBoolean, IsMongoId } from "class-validator";
 
-export interface Course {
+export class Course {
+  @IsString()
+  @IsMongoId()
   _id: string;
-  seqNo:number;
-  url:string;
+  @IsInt({ message: "seqNo must be unmeric" })
+  seqNo: number;
+  @IsString({ always: false })
+  url: string;
+  @IsString()
   iconUrl: string;
+  @IsString()
   courseListIcon: string;
+  @IsString()
   description: string;
+  @IsString()
   longDescription?: string;
+  @IsString()
   category: string;
+  @IsInt()
   lessonsCount: number;
+  @IsBoolean()
   promo: boolean;
 }
 
-
-export function compareCourses(c1:Course, c2: Course) {
-
+export function compareCourses(c1: Course, c2: Course) {
   const compare = c1.seqNo - c2.seqNo;
 
   if (compare > 0) {
     return 1;
-  }
-  else if ( compare < 0) {
+  } else if (compare < 0) {
     return -1;
-  }
-  else return 0;
-
+  } else return 0;
 }
